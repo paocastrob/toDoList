85280f9a91aafb2a594fe1606ce53bd4a70a45e9
PaolaC <paocastrob@gmail.com>
2019-07-30 18:47:27 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	notes = [];
+	add() {
+		let input1 = document.querySelector("#input1");
+		this.notes.push(input1.value);
+		input1.value = "";
+		this.forceUpdate();
+	}
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div>
+				<h1>Todos</h1>
+				<input id="input1" type="text" />
+				<input type="button" value="Enter" onClick={() => this.add()} />
+				{this.notes.map((e, i) => {
+					return (
+						<div>
+							<div key={i}>{e}</div>
+							<input
+								type="button"
+								value="delete"
+								onClick={() => {
+									this.notes.splice(i, 1);
+									this.forceUpdate();
+								}}
+							/>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
